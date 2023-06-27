379292aa3c6ae7cb863f6efda11665ca911a0ed7
Anapat Muangchol <anapat.mua@truedigital.com>
2023-06-27 17:11:49 +0700

    add test file

---
+++ tests/MyContracts.test.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('MyContract', () => {
+    it('should return its name', async () => {
+        // --- Given ---
+        const name = 'My Contract'
+
+        // --- When ---
+        const MyContract = await ethers.getContractFactory('MyContract');
+        const myContract = await MyContract.deploy(name);
+
+        await myContract.deployed();
+
+        // --- Then ---
+        expect(await myContract.getName()).to.equal(name)
+    })
+
+    it('should change its name when requested', async () => {
+        // --- Given ---
+        const oldName = 'My Contract'
+        const newName = 'Another Contract'
+
+        // --- When ---
+        const MyContract = await ethers.getContractFactory('MyContract');
+        const myContract = await MyContract.deploy(oldName);
+
+        // await myContract.deployed();
+        await myContract.setName(newName);
+
+        // --- Then ---
+        expect(await myContract.getName()).to.equal(newName)
+    })
+})
