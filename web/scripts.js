f2cafb6f4d299b424f55908c52e1d74521f862b5
Anapat Muangchol <anapat.mua@truedigital.com>
2023-07-09 20:16:06 +0700

    add web

---
+++ web/scripts.js
@@ -0,0 +1,43 @@
+// Wait for the page to load
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if Metamask is installed
+    if (typeof window.ethereum !== "undefined") {
+        const connectButton = document.getElementById("connectBtn");
+  
+        // Handle the connect button click
+        connectButton.addEventListener("click", async function() {
+            try {
+                // Request account access
+                const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+  
+                // Once the user approves the connection, the first account is available in the accounts array
+                const account = accounts[0];
+
+                document.getElementById("wallet-address").innerHTML = `Your wallet address is ${account}`;
+                alert(`Connected to Metamask! Your wallet address is ${account}`);
+            } catch (error) {
+                console.error(error);
+                alert("An error occurred while connecting to Metamask.");
+            }
+        });
+
+        const disconnectButton = document.getElementById("disconnectBtn");
+        disconnectButton.addEventListener("click", async function() {
+            if (window.ethereum.isConnected()) {
+                try {
+                    // Disconnect from Metamask
+                    await window.ethereum.send({ method: 'eth_logout' });
+                    alert('Disconnected from Metamask');
+                } catch (error) {
+                    console.error(error);
+                    alert('An error occurred while disconnecting from Metamask');
+                }
+            } else {
+                alert('Metamask is disconnected');
+            }
+        });
+
+    } else {
+        alert('Metamask is not install')
+    }
+});
